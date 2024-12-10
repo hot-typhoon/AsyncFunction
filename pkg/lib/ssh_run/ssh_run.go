2b5fa34292955a259a89023c3f7df03b7587f2d3
dsx137 <1419696375@qq.com>
2024-12-10 22:51:44 +0800

    fix: jumper

--- pkg/lib/ssh_run/ssh_run.go
+++ pkg/lib/ssh_run/ssh_run.go
@@ -28,42 +28,58 @@ func GetSSHConfig(username, password string) *ssh.ClientConfig {
 	}
 }
 
+func CreateNewSSHClient(meta SSHMeta) (*ssh.Client, error) {
+	client, err := ssh.Dial("tcp", meta.Address, GetSSHConfig(meta.Username, meta.Password))
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial: %v", err)
+	}
+	return client, nil
+}
+
+func CreateClientFromClient(meta SSHMeta, client *ssh.Client) (*ssh.Client, error) {
+	conn, err := client.Dial("tcp", meta.Address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial: %v", err)
+	}
+
+	clientConn, chans, reqs, err := ssh.NewClientConn(conn, meta.Address, GetSSHConfig(meta.Username, meta.Password))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to target: %v", err)
+	}
+	return ssh.NewClient(clientConn, chans, reqs), nil
+}
+
 func ConsumeSession(target SSHMeta, jumpers []SSHMeta, consumer func(*ssh.Session) (string, error)) (string, error) {
-	client := (*ssh.Client)(nil)
+	client, err := (*ssh.Client)(nil), error(nil)
 	for _, meta := range jumpers {
 		if client == nil {
-			_client, err := ssh.Dial("tcp", meta.Address, GetSSHConfig(meta.Username, meta.Password))
+			client, err = CreateNewSSHClient(meta)
 			if err != nil {
-				return "", fmt.Errorf("failed to dial: %v", err)
+				return "", fmt.Errorf("failed to create new client: %v", err)
 			}
-			if err != nil {
-				return "", err
-			}
-			defer _client.Close()
-			client = _client
+			defer client.Close()
 			continue
 		}
 
-		conn, err := client.Dial("tcp", meta.Address)
+		client, err = CreateClientFromClient(meta, client)
 		if err != nil {
-			return "", fmt.Errorf("failed to dial: %v", err)
+			return "", fmt.Errorf("failed to create new client: %v", err)
 		}
-
-		clientConn, chans, reqs, err := ssh.NewClientConn(conn, meta.Address, GetSSHConfig(meta.Username, meta.Password))
-		if err != nil {
-			return "", fmt.Errorf("failed to connect to target: %v", err)
-		}
-		client = ssh.NewClient(clientConn, chans, reqs)
 		defer client.Close()
 	}
 
 	if client == nil {
-		_client, err := ssh.Dial("tcp", target.Address, GetSSHConfig(target.Username, target.Password))
+		client, err = CreateNewSSHClient(target)
 		if err != nil {
-			return "", fmt.Errorf("failed to dial: %v", err)
+			return "", fmt.Errorf("failed to create new client: %v", err)
 		}
-		defer _client.Close()
-		client = _client
+		defer client.Close()
+	} else {
+		client, err = CreateClientFromClient(target, client)
+		if err != nil {
+			return "", fmt.Errorf("failed to create new client: %v", err)
+		}
+		defer client.Close()
 	}
 
 	session, err := client.NewSession()
