bd9f2d83a5a09ca2edef251b8bdfe9232fa020b1
dsx137 <logenicite@outlook.com>
2024-12-10 22:20:39 +0800

    feature: ssh_run

---
+++ pkg/lib/ssh_run/ssh_run.go
@@ -0,0 +1,66 @@
+package ssh_run
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type SSHMeta struct {
+	Address  string
+	Username string
+	Password string
+}
+
+type BodyParams struct {
+	Command string    `json:"command"`
+	Target  SSHMeta   `json:"target"`
+	Jumpers []SSHMeta `json:"jumpers"`
+}
+
+func GetSSHConfig(username, password string) *ssh.ClientConfig {
+	return &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+}
+
+func ConsumeSession(target SSHMeta, jumpers []SSHMeta, consumer func(*ssh.Session) (string, error)) (string, error) {
+	client := (*ssh.Client)(nil)
+	for _, meta := range jumpers {
+		if client == nil {
+			_client, err := ssh.Dial("tcp", meta.Address, GetSSHConfig(meta.Username, meta.Password))
+			if err != nil {
+				return "", fmt.Errorf("failed to dial: %v", err)
+			}
+			if err != nil {
+				return "", err
+			}
+			defer _client.Close()
+			client = _client
+			continue
+		}
+
+		conn, err := client.Dial("tcp", meta.Address)
+		if err != nil {
+			return "", fmt.Errorf("failed to dial: %v", err)
+		}
+
+		clientConn, chans, reqs, err := ssh.NewClientConn(conn, meta.Address, GetSSHConfig(meta.Username, meta.Password))
+		if err != nil {
+			return "", fmt.Errorf("failed to connect to target: %v", err)
+		}
+		client = ssh.NewClient(clientConn, chans, reqs)
+		defer client.Close()
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("failed to create session: %v", err)
+	}
+
+	return consumer(session)
+}
