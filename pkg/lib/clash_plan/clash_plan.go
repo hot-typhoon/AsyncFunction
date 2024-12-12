e277b419cdca813f1ae08c14bba7e8204905f587
dsx137 <1419696375@qq.com>
2024-12-12 08:55:57 +0800

    feature: clash_plan

---
+++ pkg/lib/clash_plan/clash_plan.go
@@ -0,0 +1,54 @@
+package clash_plan
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"asynclab.club/AsyncFunction/pkg/util"
+)
+
+type QueryParams struct {
+	Url string
+}
+
+type ClashPlan struct {
+	Upload   string `json:"upload"`
+	Download string `json:"download"`
+	Total    string `json:"total"`
+	Expire   string `json:"expire"`
+}
+
+func GetClashPlan(url string) (*ClashPlan, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "clash-verge/v2.0.2")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: %v", resp.Status)
+	}
+
+	info := resp.Header.Get("Subscription-Userinfo")
+	if info == "" {
+		return nil, fmt.Errorf("error: %v", "Subscription-Userinfo not found")
+	}
+
+	var upload, download, total, expire int
+	fmt.Sscanf(info, "upload=%d; download=%d; total=%d; expire=%d", &upload, &download, &total, &expire)
+
+	return &ClashPlan{
+		Upload:   util.ConvertBytesToHuman(upload),
+		Download: util.ConvertBytesToHuman(download),
+		Total:    util.ConvertBytesToHuman(total),
+		Expire:   time.Unix(int64(expire), 0).Local().Format("2006-01-02 15:04:05"),
+	}, nil
+}
